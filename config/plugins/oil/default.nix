fa15d10578409261529b01d21826d6312326a695
Eric Zhang <me@ericzhang.dev>
2025-01-31 08:14:19 -0500

    :bug:

--- config/plugins/oil/default.nix
+++ config/plugins/oil/default.nix
@@ -2,6 +2,9 @@
   plugins.oil = {
     enable = true;
     settings = {
+      keymaps = {
+        "\'" = "actions.parent";
+      };
       view_options.show_hidden = true;
       skip_confirm_for_simple_edits = true;
     };
